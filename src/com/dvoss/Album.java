29f082a5d8a98319327db82c3363270de60bdc1a
Dan <danielandrewvoss@gmail.com>
2016-05-25 12:30:42 -0400

    Create File I/O program

---
+++ src/com/dvoss/Album.java
@@ -0,0 +1,36 @@
+package com.dvoss;
+
+/**
+ * Created by Dan on 5/25/16.
+ */
+public class Album {
+
+    String title;
+    String artist;
+    String genre;
+    int songNumber;
+    int year;
+
+    public Album() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public int getSongNumber() {
+        return songNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+}
